0a077697f6af9379cd29832372fc2861f3bfdf8a
Parker Parham <parkerparham@gmail.com>
2022-12-05 14:07:51 -0500

    Adds Hero, Navbar, and Footer components

---
+++ src/components/Hero.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Hero() {
+  return (
+    <>
+    <h1>Hello, my name is <span>Parker Parham</span>, and I am a Web Developer.</h1>
+    </>
+  )
+}
