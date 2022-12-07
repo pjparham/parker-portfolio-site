fc10215e320d1d50dd98d57f75193cf1fda7126a
Parker Parham <parkerparham@gmail.com>
2022-12-07 15:39:28 -0500

    Installs package from emailjs and enables form to send email when submit is clicked

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -1,15 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import { css } from '@emotion/css'
+import { send } from 'emailjs-com';
 
 export default function ContactMe() {
-    const [value, setValue] = React.useState('Controlled');
-    const handleChange = (event) => {
-        setValue(event.target.value);
-      };
-      console.log(value)
+    const [value, setValue] = useState({
+        name: "",
+        email: "",
+        company: "",
+        message: "",
+    });
+    function handleChange(e){
+        setValue({
+            ...value,
+            [e.target.name]: e.target.value,
+        })
+    }
+    // const onSubmit = (e) => {
+    //     e.preventDefault();
+    //     send(
+    //       'SERVICE ID',
+    //       'TEMPLATE ID',
+    //       toSend,
+    //       'User ID'
+    //     )
+    //       .then((response) => {
+    //         console.log('SUCCESS!', response.status, response.text);
+    //       })
+    //       .catch((err) => {
+    //         console.log('FAILED...', err);
+    //       });
+    //   };
+
+    function handleSubmit(){
+        console.log(value)
+        send(
+            'service_i2rfgj9',
+            'template_dl5ta0b',
+            value,
+            'wimlVlaqMA5lWMJ3a'
+        )
+        .then((response) => {
+                    console.log('SUCCESS!', response.status, response.text);
+                  })
+                  .catch((err) => {
+                    console.log('FAILED...', err);
+                  });
+        setValue({
+            name: "",
+            email: "",
+            company: "",
+            message: "",
+        })
+    }
+
       return (
         <div className="pages contact">
             <h1>Contact Me:</h1>
@@ -26,6 +72,9 @@ export default function ContactMe() {
                 id="outlined-textarea"
                 label="Name"
                 placeholder="Name"
+                name="name"
+                value={value.name}
+                onChange={handleChange}
                 variant="filled"
                 className={css`
                 background-color: white;
@@ -36,6 +85,9 @@ export default function ContactMe() {
                 label="E-Mail"
                 placeholder="E-Mail"
                 variant="filled"
+                name="email"
+                value={value.email}
+                onChange={handleChange}
                 className={css`
                 background-color: white;
                 `}
@@ -45,6 +97,9 @@ export default function ContactMe() {
                 label="Company"
                 placeholder="Company"
                 variant="filled"
+                name="company"
+                value={value.company}
+                onChange={handleChange}
                 className={css`
                 background-color: white;
                 `}
@@ -55,14 +110,17 @@ export default function ContactMe() {
                 variant="filled"
                 multiline
                 rows={4}
-                defaultValue="Please include additional info here. I look forward to connecting with you!"
+                placeholder="Please include additional info here. I look forward to connecting with you!"
+                name="message"
+                value={value.message}
+                onChange={handleChange}
                 className={css`
                 background-color: white;
                 `}
                 />
             </div>
                 <div className="contact-button-container">
-                    <div className="contact-button">Submit</div>
+                    <div onClick={handleSubmit} className="contact-button">Submit</div>
                 </div>
             </Box>
         </div>
