961d3ae55162c8bd2312b8d36d21018b73b70c9d
Parker Parham <parkerparham@gmail.com>
2022-12-07 14:04:32 -0500

    Adds caption to images in project description

--- src/components/Project.js
+++ src/components/Project.js
@@ -10,10 +10,12 @@ export default function Project({ project }) {
   return (
     <div className="project-card">
         <h3>{project.title}</h3>
+
         <div className="project-image-container">
             <img src={photos[project.mobileImage]} alt="Parker' project in mobile view" className="project-mobile-image"/>
             <img src={photos[project.desktopImage]} alt="Parker' project in desktop view" className="project-desktop-image"/>
         </div>
+        <p className="project-image-caption">Pictured: Mobile and Desktop views of {project.title}</p>
         <p><b>Descripton:</b></p>
         <p>{project.description}</p>
         <p><b>Tools used:</b></p>
@@ -22,6 +24,7 @@ export default function Project({ project }) {
             <a className="project-link" href={project.videoLink}>Video Demo  <YouTubeIcon/></a><br/>
             <a className="project-link" href={project.githubLink}>Github Repository  <GitHubIcon/></a>
         </div>
+        <hr/>
     </div>
   )
 }
